7da34fcbafa35f75f0e1e3b328fb53cb02a9439b
Wei <zero.maxima@gmail.com>
2020-06-08 18:39:57 +0800

    [Update] Add callback to task

--- coroutineSample.py
+++ coroutineSample.py
@@ -1,15 +1,20 @@
 import time
 import asyncio
+from asyncio import Task
 
-now = lambda : time.time()
+now = lambda: time.time()
 
 
 async def do_some_work(x):
     print("waiting: ", x)
+    return "Done after {}s".format(x)
+
+
+def callback(future: Task):
+    print("callback: ", future.result())
 
 
 if __name__ == '__main__':
-
     start = now()
 
     # Declare a coroutine but not execute
@@ -19,13 +24,15 @@ if __name__ == '__main__':
     loop = asyncio.get_event_loop()
 
     # Create Task from coroutine
-    # task = loop.create_task(coroutine)
-    task = asyncio.ensure_future(coroutine)
-    print(task) # Pending task
+    task = loop.create_task(coroutine)
+    print(task)  # Pending task
+
+    # Add callback
+    task.add_done_callback(callback)
+    print(task)
 
     # Add task to event loop
     loop.run_until_complete(task)
-    print(task) # Finished task
+    print(task)  # Finished task
 
     print("Time: ", now() - start)
-
