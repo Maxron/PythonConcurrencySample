eb2416b30c9b2ec33ecc2494f86fb5793090cda7
Wei <zero.maxima@gmail.com>
2020-06-09 16:44:18 +0800

    [Update] Add await usage as blocking

--- coroutineSample.py
+++ coroutineSample.py
@@ -7,6 +7,7 @@ now = lambda: time.time()
 
 async def do_some_work(x):
     print("waiting: ", x)
+    await asyncio.sleep(x) # Blocking
     return "Done after {}s".format(x)
 
 
@@ -19,20 +20,14 @@ if __name__ == '__main__':
 
     # Declare a coroutine but not execute
     coroutine = do_some_work(2)
-
     # Create a event loop
     loop = asyncio.get_event_loop()
-
     # Create Task from coroutine
     task = loop.create_task(coroutine)
-    print(task)  # Pending task
-
     # Add callback
     task.add_done_callback(callback)
-    print(task)
-
     # Add task to event loop
     loop.run_until_complete(task)
-    print(task)  # Finished task
 
+    print("Task ret: ", task.result())
     print("Time: ", now() - start)
