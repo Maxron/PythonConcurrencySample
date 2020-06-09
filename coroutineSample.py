d49bedaa03f0c0ec4f13b0f1c5a9dc7a3d6e6964
Wei <zero.maxima@gmail.com>
2020-06-09 18:21:21 +0800

    [Update] Add nested async usage

--- coroutineSample.py
+++ coroutineSample.py
@@ -7,7 +7,7 @@ now = lambda: time.time()
 
 async def do_some_work(x):
     print("waiting: ", x)
-    await asyncio.sleep(x) # Blocking
+    await asyncio.sleep(x)  # Blocking
     return "Done after {}s".format(x)
 
 
@@ -15,28 +15,29 @@ def callback(future: Task):
     print("callback: ", future.result())
 
 
-if __name__ == '__main__':
-    start = now()
-
+async def main():
     coroutine1 = do_some_work(4)
     coroutine2 = do_some_work(2)
     coroutine3 = do_some_work(1)
 
-    task1 = asyncio.ensure_future(coroutine1)
-    task1.add_done_callback(callback)
+    tasks = [
+        asyncio.ensure_future(coroutine1),
+        asyncio.ensure_future(coroutine2),
+        asyncio.ensure_future(coroutine3)
+    ]
 
-    task2 = asyncio.ensure_future(coroutine2)
-    task2.add_done_callback(callback)
+    dones, pendings = await asyncio.wait(tasks)
+    for task in dones:
+        print("Task ret:", task.result())
 
-    task3 = asyncio.ensure_future(coroutine3)
-    task3.add_done_callback(callback)
+    # results = await asyncio.gather(*tasks)
+    # for result in results:
+    #     print("Tasks ret:", result)
 
-    tasks = [task1, task2, task3]
+if __name__ == '__main__':
+    start = now()
 
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(asyncio.gather(task1, task2, task3))
-
-    for task in tasks:
-        print("Task ret:", task.result())
+    loop.run_until_complete(main())
 
     print("Time: ", now() - start)
